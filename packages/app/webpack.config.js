003f06c11015535e4666fe0245d5e18d3a515b98
Lucas Barga <lucas.barga@zup.com.br>
2021-06-03 18:33:23 -0300

    finally app root

--- packages/app/webpack.config.js
+++ packages/app/webpack.config.js
@@ -1,1 +1,44 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
+const { dirname } = path
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: '[name]'.js,
+        path: path.resolve(__dirname, './dist'),
+        publicPath: 'http://localhost:3000/'
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, './dist'),
+        index: 'index.html',
+        port: 3000,
+        historyApiFallback: true
+    },
+    resolve: {
+        extensions: ['.jsx', '.js', '.json']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                loader: require.resolve('babel-loader'),
+                options: {
+                    presets: [require.resolve('@babel/preset-react')]
+                }
+            },
+            {
+                test: '/\.css$/',
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './public/index.html',
+            title: 'app'
+        })
+    ]
+}
